a137732c129de738a1635b91955ec6bd17b8f6ce
LUIS ENRIQUE MEDINA MURILLO <luisenrique.medina@ikasle.egibide.org>
2024-03-25 20:02:02 +0100

    ..

--- OnlineShop/DemoModel/Producto.swift
+++ OnlineShop/DemoModel/Producto.swift
@@ -31,10 +31,10 @@ struct ProductDemo: Identifiable {
     }
 }
 
-struct Order:Codable{
-        @DocumentID var identify:String?
-        var products: [ProductDemo]=[MockData.sampleProduct1,MockData.sampleProduct3]
-    }
+struct Order: Codable{
+    @DocumentID var identify:String?
+    var products: [ProductDemo]=[MockData.sampleProduct1,MockData.sampleProduct3]
+}
 
 
 struct MockData{
@@ -64,5 +64,63 @@ struct MockData{
                                      category: "men's clothing",
                                      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
                                      )
+
     static let products = [sampleProduct1, sampleProduct2, sampleProduct3]
+
 }
+
+/**
+ 
+ struct ProductDemo: Identifiable, Codable{
+     let id: Int
+     var title:String
+     var price: Double
+     var description: String
+     var category: String
+     var image: String
+     
+ }
+
+ struct Order:Codable{
+     @DocumentID var identify:String?
+     var products: [ProductDemo]=[MockData.sampleProduct1,MockData.sampleProduct3]
+ }
+
+
+ struct MockData{
+
+     //static let rating = Rating(rate: 3.9, count: 120)
+
+     static let sampleProduct1 = ProductDemo(id: 0001,
+                                      title: "Fjallraven Foldsack No. 1 Backpack, Fits 15 Laptops",
+                                      price: 109.95,
+                                      description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+                                      category: "men's clothing",
+                                      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                                      )
+
+     static let sampleProduct2 = ProductDemo(id: 0002,
+                                      title: "Fjallraven Foldsack No. 1 Backpack, Fits 15 Laptops",
+                                      price: 109.95,
+                                      description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+                                      category: "men's clothing",
+                                      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                                      )
+
+     static let sampleProduct3 = ProductDemo(id: 0003,
+                                      title: "Fjallraven Foldsack No. 1 Backpack, Fits 15 Laptops",
+                                      price: 109.95,
+                                      description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+                                      category: "men's clothing",
+                                      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                                      )
+
+     static let products = [sampleProduct1, sampleProduct2, sampleProduct3]
+
+ }
+
+ 
+ 
+ 
+ 
+ */
